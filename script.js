b7c9e1f95bdf17caf56cd560e17336d2729ee661
Ihar Sazanavets <iharsazanavets@gmail.com>
2024-08-31 18:44:39 +0300

    Add option to download a link via context menu

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const downloadPage = (title, url) => {
-  const html = `
+const download = (title, url) => {
+  const getHtmlContent = (title, url) => `
     <!DOCTYPE html>
     <html xmlns="http://www.w3.org/1999/xhtml">
         <head>
@@ -15,29 +15,50 @@ const downloadPage = (title, url) => {
         </body>
     </html>
     `;
+
+  const saveHtml = (fileName, htmlContent, element) => {
+    element.setAttribute(
+      "href",
+      "data:text/plain;charset=utf-8," + encodeURIComponent(htmlContent)
+    );
+    element.setAttribute("download", fileName + ".html");
+    element.click();
+  };
+
   const element = document.createElement("a");
-  element.setAttribute(
-    "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(html)
-  );
-  element.setAttribute("download", title + ".html");
-  element.style.display = "none";
-  document.body.appendChild(element);
-  element.click();
-  // we don't have to remove the element because the page itself is going to be closed
+  const htmlContent = getHtmlContent(title, url);
+  saveHtml(title, htmlContent, element);
 };
 
-chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith("http")) {
-    try {
-      await chrome.scripting.executeScript({
-        target: { tabId: tab.id },
-        func: downloadPage,
-        args: [tab.title, decodeURI(tab.url)],
-      });
-      chrome.tabs.remove(tab.id);
-    } catch (e) {
-      console.log(e);
-    }
+const processLink = async (title, url, tabId, closeTab) => {
+  if (!title || !url || !url.startsWith("http") || !tabId) {
+    return;
   }
+  try {
+    await chrome.scripting.executeScript({
+      target: { tabId },
+      func: download,
+      args: [title, url],
+    });
+    if (closeTab) {
+      chrome.tabs.remove(tabId);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+chrome.action.onClicked.addListener((tab) =>
+  processLink(tab.title, decodeURI(tab.url), tab.id, true)
+);
+
+chrome.contextMenus.onClicked.addListener(
+  ({ selectionText, linkUrl }, { id }) =>
+    processLink(selectionText, decodeURI(linkUrl), id)
+);
+
+chrome.contextMenus.create({
+  id: "save-link",
+  title: "Save this link",
+  contexts: ["link"],
 });
