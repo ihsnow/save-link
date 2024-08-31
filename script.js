b8e94b3e92cd3d80ede62f429552bc4030651e53
Ihar Sazanavets <iharsazanavets@gmail.com>
2024-08-31 18:08:15 +0300

    Add option to download a link via context menu

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-const handleClick = (tab) => {
-  if (!tab.url.startsWith("http")) {
-    return;
-  }
+const element = document.createElement("a");
 
-  const title = tab.title;
-  const url = decodeURI(tab.url);
-
-  const html = `
+const getHtmlContent = (title, url) => `
     <!DOCTYPE html>
     <html xmlns="http://www.w3.org/1999/xhtml">
         <head>
@@ -23,18 +17,42 @@ const handleClick = (tab) => {
     </html>
     `;
 
-  const element = document.createElement("a");
+const saveHtml = (fileName, htmlContent) => {
   element.setAttribute(
     "href",
-    "data:text/plain;charset=utf-8," + encodeURIComponent(html)
+    "data:text/plain;charset=utf-8," + encodeURIComponent(htmlContent)
   );
-  element.setAttribute("download", tab.title + ".html");
-  element.style.display = "none";
-  document.body.appendChild(element);
+  element.setAttribute("download", fileName + ".html");
   element.click();
-  // we don't have to remove the element because the page itself is going to be closed
-
-  browser.tabs.remove(tab.id);
+  element.removeAttribute("href");
+  element.removeAttribute("download");
 };
 
-browser.action.onClicked.addListener(handleClick);
+const processLink = (title, url, tabId, closeTab) => {
+  if (!title || !url || !url.startsWith("http") || !tabId) {
+    return;
+  }
+  try {
+    const htmlContent = getHtmlContent(title, url);
+    saveHtml(title, htmlContent);
+    if (closeTab) {
+      browser.tabs.remove(tabId);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+browser.action.onClicked.addListener((tab) =>
+  processLink(tab.title, decodeURI(tab.url), tab.id, true)
+);
+
+browser.contextMenus.onClicked.addListener(({ linkText, linkUrl }, { id }) =>
+  processLink(linkText, decodeURI(linkUrl), id)
+);
+
+browser.contextMenus.create({
+  id: "save-link",
+  title: "Save this link",
+  contexts: ["link"],
+});
